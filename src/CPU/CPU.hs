3be4cfb014b329448099a2c1cbad89d361f2c078
suppi <soupiral@gmail.com>
2015-08-09 00:14:38 +0300

    Now using microlens instead of record syntax

--- src/CPU/CPU.hs
+++ src/CPU/CPU.hs
@@ -1,49 +1,53 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module CPU.CPU
 where
 
 import qualified Data.Word as W (Word8,Word16)
 import qualified Data.Vector.Unboxed as V
+import Lens.Micro.TH (makeLenses)
+import qualified Lens.Micro.Mtl as Lens (view)
 
 -- |
 -- Modeling a CHIP-8 CPU
-data CPU = CPU { opcode       :: W.Word16
-               , index        :: W.Word16
-               , pc           :: W.Word16
-               , sp           :: W.Word8
-               , delayTimer   :: W.Word8
-               , soundTimer   :: W.Word8
-               , stack        :: V.Vector W.Word8
-               , registers    :: V.Vector W.Word8
-               , keypad       :: V.Vector W.Word8
-               , memory       :: V.Vector W.Word8
-               , gfx          :: V.Vector W.Word8
+data CPU = CPU { _opcode       :: W.Word16
+               , _index        :: W.Word16
+               , _pc           :: W.Word16
+               , _sp           :: W.Word8
+               , _delayTimer   :: W.Word8
+               , _soundTimer   :: W.Word8
+               , _stack        :: V.Vector W.Word8
+               , _registers    :: V.Vector W.Word8
+               , _keypad       :: V.Vector W.Word8
+               , _memory       :: V.Vector W.Word8
+               , _gfx          :: V.Vector W.Word8
                } deriving (Read, Show, Eq)
 
+makeLenses ''CPU
 
 -- |
 -- initializing the CPU with 0
 initCPU :: CPU
-initCPU = CPU { opcode         = 0
-              , index          = 0
-              , pc             = 0
-              , sp             = 0
-              , delayTimer     = 0
-              , soundTimer     = 0
-              , stack          = V.replicate 16 0
-              , registers      = V.replicate 16 0
-              , keypad         = V.replicate 16 0
-              , memory         = V.replicate 4096 0
-              , gfx            = V.replicate (64 * 32) 0
+initCPU = CPU { _opcode         = 0
+              , _index          = 0
+              , _pc             = 0
+              , _sp             = 0
+              , _delayTimer     = 0
+              , _soundTimer     = 0
+              , _stack          = V.replicate 16 0
+              , _registers      = V.replicate 16 0
+              , _keypad         = V.replicate 16 0
+              , _memory         = V.replicate 4096 0
+              , _gfx            = V.replicate (64 * 32) 0
               }
 
 -- |
 -- Returns the program counter as Int
 getPC :: CPU -> Int
-getPC = fromIntegral . pc
+getPC = fromIntegral . Lens.view pc
 
 -- |
 -- Returns the stack pointer as Int
 getSP :: CPU -> Int
-getSP = fromIntegral . sp
+getSP = fromIntegral . Lens.view sp
--- src/CPU/Emulate.hs
+++ src/CPU/Emulate.hs
@@ -9,6 +9,9 @@ module CPU.Emulate where
 import qualified Data.Word as W (Word8, Word16)
 import qualified Data.Vector.Unboxed as V
 -- import Data.Bits ((.&.))
+import Lens.Micro ((&))
+import qualified Lens.Micro     as Lens
+import qualified Lens.Micro.Mtl as Lens
 
 import Utils
 import CPU.CPU (CPU)
@@ -37,7 +40,7 @@ fetch cpu =
     Nothing ->
       throwErr "Program counter out of bounds"
 
-  where cmd = Bits.merge <$> (CPU.memory cpu V.!? CPU.getPC cpu) <*> (CPU.memory cpu V.!? (CPU.getPC cpu + 1))
+  where cmd = Bits.merge <$> (Lens.view CPU.memory cpu V.!? CPU.getPC cpu) <*> (Lens.view CPU.memory cpu V.!? (CPU.getPC cpu + 1))
 
 decode :: W.Word16 -> Emulate Instruction
 decode cmd =
@@ -53,9 +56,8 @@ execute = flip ($)
 
 updateTimers :: CPU -> CPU
 updateTimers cpu =
-  cpu { CPU.delayTimer = if CPU.delayTimer cpu > 0 then CPU.delayTimer cpu - 1 else 0
-      , CPU.soundTimer = if CPU.soundTimer cpu > 0 then CPU.soundTimer cpu - 1 else 0
-      }
+  cpu & Lens.over CPU.delayTimer (\dt -> if dt > 0 then dt - 1 else 0)
+      & Lens.over CPU.soundTimer (\st -> if st > 0 then st - 1 else 0)
 
 ----------------
 -- update CPU
@@ -65,30 +67,29 @@ updateTimers cpu =
 -- increases the program counter by 2
 nextPC :: CPU -> Emulate CPU
 nextPC cpu =
-  pure cpu { CPU.pc = CPU.pc cpu + 2 }
+  pure $ Lens.over CPU.pc (+2) cpu
 
 -- |
 -- store a value on the stack, updating the stack and the stack pointer.
 -- fail if out of bounds
 storeOnStack :: W.Word8 -> CPU -> Emulate CPU
 storeOnStack val cpu =
-  if CPU.getSP cpu >= V.length (CPU.stack cpu)
+  if CPU.getSP cpu >= V.length (Lens.view CPU.stack cpu)
   then throwErr "The stack is full"
-  else pure
-    cpu { CPU.stack = CPU.stack cpu V.// [(CPU.getSP cpu, val)]
-        , CPU.sp = CPU.sp cpu + 1
-        }
+  else pure $
+    cpu & Lens.over CPU.stack (V.// [(CPU.getSP cpu, val)])
+        & Lens.over CPU.sp (+1)
 
 -- |
 -- pops a value from the stack, updating the stack pointer.
 -- fail if out of bounds
 popFromStack :: CPU -> Emulate (W.Word8, CPU)
 popFromStack cpu =
-  if CPU.getSP cpu <= 0 && CPU.getSP cpu >= V.length (CPU.stack cpu)
+  if CPU.getSP cpu <= 0 && CPU.getSP cpu >= V.length (Lens.view CPU.stack cpu)
   then
     throwErr "The stack is full"
   else
-    pure (CPU.stack cpu V.! CPU.getSP cpu, cpu { CPU.sp = CPU.sp cpu - 1 })
+    pure (Lens.view CPU.stack cpu V.! CPU.getSP cpu, Lens.over CPU.sp (\x -> x-1) cpu)
 
 
 -------------
